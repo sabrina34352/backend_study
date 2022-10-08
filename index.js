aed331d04b5dcbbff9578104ceba94399859fe0c
sabrina34352 <s.babakulova@student.inha.uz>
2022-10-09 00:05:09 +0500

    fix to console arguments

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-  let numArray = process.argv[2].split(',').map(Number);
-  if (numArray.every((num) => Number.isNaN(num))) {
-    console.log('Please enter array of numbers only\n');
-  } else {
-    const sum = numArray.reduce((a, b) => a + b, 0);
-    console.log(`The sum of the numbers is ${sum}`);
-    return;
-  }
+let numArray = process.argv.slice(2);
 
+if (numArray.length === 1) {
+  numArray = process.argv[2].split(',');
+}
+
+if (numArray.some((num) => Number.isNaN(num)) || numArray.length === 0) {
+  console.log('Please enter array of numbers in correct format\n');
+} else {
+  const sum = numArray.map(Number).reduce((a, b) => a + b, 0);
+  console.log(`The sum of the numbers is ${sum}`);
+  return;
+}
